c523730c951059743a4cb2cc84c67b02e9357042
Leandro Fialho <leandroaparecido13@yahoo.com.br>
2019-09-20 16:06:30 -0300

    Ajustes no javaScript do botão

--- src/js/main.js
+++ src/js/main.js
@@ -463,12 +463,10 @@
   class backTopButton {
     constructor(topButton) {
       this.topButton = document.querySelector(topButton);
-      console.log(this.topButton);
       this.showButton = debounce(this.showButton.bind(this), 50);
     }
 
     showButton() {
-      console.log('test');
       if (window.window.pageYOffset > 100) this.topButton.classList.add('active');else this.topButton.classList.remove('active');
     }
 
