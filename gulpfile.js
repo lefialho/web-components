1f65bd1a95fbeb99f6c38f43ed0b5b4354cb71fc
Leandro Fialho <leandroaparecido13@yahoo.com.br>
2019-09-27 11:27:14 -0300

    Alteração de caminho dos arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const browserSync = require('browser-sync').create();
 const pug = require('gulp-pug');
 const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass');
-const clean = require('gulp-clean');
+// const clean = require('gulp-clean');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify')
@@ -22,12 +22,12 @@ const commonjs = require('rollup-plugin-commonjs');
 // -----------------
 gulp.task('browserSync', function () {
   browserSync.init({
-    server: './src'
+    server: './'
   });
   gulp.watch('src/views/**/*.pug', ['pug']);
   gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch(['src/js/**/*.js', '!src/js/scripts.min.js'], ['js']);
-  gulp.watch('src/*.html').on('change', browserSync.reload);
+  gulp.watch(['src/js/**/*.js'], ['js']);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
   gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
 });
 
@@ -36,7 +36,7 @@ gulp.task('pug', function () {
     .pipe(pug({
       pretty: true
     })) //Convert to HTML, but no minify
-    .pipe(gulp.dest('src'))
+    .pipe(gulp.dest('./'))
     .pipe(browserSync.stream())
 });
 
@@ -49,18 +49,18 @@ gulp.task('sass', function () {
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(gulp.dest('src/css'))
+    .pipe(gulp.dest('./css'))
     .pipe(browserSync.reload({
       stream: true
     }))
 });
 
-gulp.task('clean-scripts', function () {
-  return gulp.src('src/js/scripts.min.js', {
-      read: false
-    })
-    .pipe(clean())
-});
+// gulp.task('clean-scripts', function () {
+//   return gulp.src('src/js/scripts.min.js', {
+//       read: false
+//     })
+//     .pipe(clean())
+// });
 
 gulp.task('export-js', function () {
   return gulp.src('src/js/components/main.js')
@@ -70,14 +70,14 @@ gulp.task('export-js', function () {
     .pipe(gulp.dest('src/js'))
 });
 
-gulp.task('js', ['export-js','clean-scripts'], function () {
+gulp.task('js', ['export-js'], function () {
   return gulp.src(['src/js/main.js', '!src/js/components/clipboard.js', '!src/js/lib/*.js'])
     .pipe(concat('components.js'))
     .pipe(babel({
       presets: ['@babel/env']
     }))
     .pipe(uglify())
-    .pipe(gulp.dest('src/js'))
+    .pipe(gulp.dest('./js'))
     .pipe(browserSync.stream())
 });
 
@@ -89,7 +89,7 @@ gulp.task('images', function () {
     .pipe(cache(imagemin({
       interlaced: true
     })))
-    .pipe(gulp.dest('src/img'))
+    .pipe(gulp.dest('./img'))
 });
 
 const config = {
